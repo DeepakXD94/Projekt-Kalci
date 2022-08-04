d31263e2ff9a1473cb0972697ffca7205f66f5da
DeepakXD94 <109253936+DeepakXD94@users.noreply.github.com>
2022-08-04 23:24:10 +0530

    Just intialised.

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,22 @@
-def calc(x,y,z):
+def calc(x,y):
+    if z == str('+'):
+        print(add())
+    elif z == str('-'):
+        print(sub())
+    else:
+        print(multi(x,y))
+
+def add(a,b):
+    return a + b
+    
+def sub(a,b):
+    return a - b
+
+def mult(a,b):
+    return a * b
+
+x = float(input("Enter the first number : "))
+z = input("Enter the operator")
+y = float(input("Enter the second number : "))
+
+calc(x,y)
