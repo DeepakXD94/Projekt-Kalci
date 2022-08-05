8c35d8465d8ebf8eb6faf97ef57807f17b78ddd8
DeepakXD94 <109253936+DeepakXD94@users.noreply.github.com>
2022-08-05 05:56:10 +0530

    Added comparison operators and exponentials.

--- kalkulator.py
+++ kalkulator.py
@@ -1,10 +1,33 @@
 def calc(x,y):
     if z == str('+'):
-        print(add())
+        print(add(x, y))
+
     elif z == str('-'):
-        print(sub())
+        print(sub(x, y))
+
+    elif z == str('*'):
+        print(multi(x, y))
+
+    elif z == str('/'):
+        print(div(x, y))
+
+    elif z == str('%'):
+        print(rem(x,y))
+
+    elif z == str('>'):
+        print(greater(x,y))
+
+    elif z == str('<'):
+        print(lower(x,y))
+
+    elif z == str('pwr'):
+        print(pwr(x,y))
+
+    elif z == str('root'):
+        print(root(x,y))
+        
     else:
-        print(multi(x,y))
+        print("Error. Please enter proper input.")
 
 def add(a,b):
     return a + b
@@ -15,8 +38,26 @@ def sub(a,b):
 def mult(a,b):
     return a * b
 
+def div(a,b):
+    return a / b
+
+def rem(a,b):
+    return a % b
+
+def greater(a,b):
+    return a > b
+
+def lower(a,b):
+    return a < b
+
+def pwr(a,b):
+    return a ** b
+
+def root(a,b):
+    return a ** (1/b)
+
 x = float(input("Enter the first number : "))
-z = input("Enter the operator")
+z = input("Enter the operator : ")
 y = float(input("Enter the second number : "))
 
 calc(x,y)
